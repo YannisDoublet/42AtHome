515d5933118bdb8e27b783eea9d67e8cea3e856b
Yannis Doublet <yadouble@student.42.fr>
2018-04-17 12:46:05 +0200

    Taf

---
+++ ft_strjoin.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yannisdo <yannisdo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/16 18:59:28 by yannisdo          #+#    #+#             */
+/*   Updated: 2018/04/16 18:59:30 by yannisdoublet    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	char *str;
+	size_t i;
+
+	i = 0;
+	if(!(str = malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1)))
+		return (NULL);
+	while (*s1)
+		str[i++] = *s1++;
+	while (*s2)
+		str[i++] = *s2++;
+	str[i] = '\0';
+	return (str);
+}
